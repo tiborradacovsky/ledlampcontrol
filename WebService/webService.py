2da6475a1afaffed416d4473d623ed1963ca0685
MartinMelicher <mato.melicher@gmail.com>
2017-02-26 21:39:30 +0100

    minor changes in webService (lebo mne to nešlo, aj keď ostatným vraj hej)

--- WebService/webService.py
+++ WebService/webService.py
@@ -15,23 +15,22 @@ app = web.application(urls, globals())
 
 class index:
     def GET(self):
-        print 'hi. service is up & running.'
+        print ('hi. service is up & running.')
 
 class list_lamps:
     def GET(self):
         output = 'lamps:[';
         for child in root:
-            print 'child', child.tag, child.attrib
+            print('child'), child.tag, child.attrib
             output += str(child.attrib) + ','
         output += ']';
         return output
 
 class get_lamp:
     def GET(self, lamp):
-	for child in root:
-		if child.attrib['id'] == lamp:
-		    return str(child.attrib)
+        for child in root:
+            if child.attrib['id'] == lamp:
+                return str(child.attrib)
 
 if __name__ == "__main__":
     app.run()
-    
